085f3376aaf9b53ecb0a2a453d4c14b8749df6b4
James Sherry <james.m.sherry@googlemail.com>
2021-03-10 18:17:26 +0000

    initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Scraping
+
+[Helpful article](https://www.twilio.com/blog/4-tools-for-web-scraping-in-node-js)
+
+With web scraping we make a get request for a page then turn the HTML string into a dom (via some package) then search that DOM for our results.
---
+++ server.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const got = require("got");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const URL = "https://www.thejump.tech";
+
+(async () => {
+  try {
+    // const response = await got(URL);
+    // const dom = new JSDOM(response.body);
+    const response = await axios(URL);
+    const dom = new JSDOM(response.data);
+
+    const nodeList = [
+      ...dom.window.document.querySelectorAll("h1"),
+      ...dom.window.document.querySelectorAll("h2"),
+      ...dom.window.document.querySelectorAll("h3"),
+    ];
+
+    nodeList.forEach((heading) => {
+      console.log(`Heading text: ${heading.textContent}`);
+    });
+    console.log(`${nodeList.length} headings found`);
+  } catch (err) {
+    console.log("err", err);
+  }
+})();
